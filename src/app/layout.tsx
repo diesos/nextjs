86bace9250691c83545240eb285c8306981bc415
Diesos <ozturk@live.Fr>
2024-10-16 23:27:29 +0200

    Refactor utility function for merging CSS classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { DockDemo } from "./Dock";
+import { Button } from "@/components/ui/button";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,8 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>
+        <nav className="hidden sm:contents">
         <ul  className="flex justify-center gap-6 py-4 rounded-lg bg-gray-300 my-4 mx-32 justify-items-center text-center text-black transition ease-in-out hover:text-gray-800 duration-200">
+          <Button>Hello</Button>
           <li><a href="/">Home</a></li>
           <li> | </li>
           <li><a href="/notes">Notes</a></li>
@@ -39,6 +42,7 @@ export default function RootLayout({
           <li><a href="/notes/addnote">Add Note</a></li>
         </ul>
       </nav>
+      <DockDemo />
       <Toaster position="bottom-center" />
         {children}
       </body>
