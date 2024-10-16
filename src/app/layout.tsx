924bce21e4525fbc5a44bb178cc7acabaa13ea4f
Diesos <ozturk@live.Fr>
2024-10-16 23:28:00 +0200

    Refactor layout component and remove unused code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
-import { DockDemo } from "./Dock";
-import { Button } from "@/components/ui/button";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -34,7 +33,6 @@ export default function RootLayout({
       >
         <nav className="hidden sm:contents">
         <ul  className="flex justify-center gap-6 py-4 rounded-lg bg-gray-300 my-4 mx-32 justify-items-center text-center text-black transition ease-in-out hover:text-gray-800 duration-200">
-          <Button>Hello</Button>
           <li><a href="/">Home</a></li>
           <li> | </li>
           <li><a href="/notes">Notes</a></li>
@@ -42,7 +40,6 @@ export default function RootLayout({
           <li><a href="/notes/addnote">Add Note</a></li>
         </ul>
       </nav>
-      <DockDemo />
       <Toaster position="bottom-center" />
         {children}
       </body>
